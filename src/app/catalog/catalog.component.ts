51fc8eb467a15d674b307af40d1680b672476c48
Isaac Silva <isaac.silva@kuehne-nagel.com>
2025-05-08 09:24:19 +0100

    added product interface

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'ibot-catalog',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  product: IProduct;
 
+  constructor() {
+    this.product = {
+      id: 1,
+      description: 'A friendly robot head with two eyes and a smile - that helps you with your tasks.',
+      name: 'Friendly Bot',
+      imageName: 'head-friendly.png',
+      category: 'Heads',
+      price: 100,
+      discount: 10
+    };
+  }
+
+  getImageUrl(product:IProduct): string {
+    return `/assets/images/robot-parts/${product.imageName}`;
+  }
 }

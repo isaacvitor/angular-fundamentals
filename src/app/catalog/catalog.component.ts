96b35b5dae4f891d8c11e1b49e3ae8dd78a67289
Isaac Silva <isaac.silva@kuehne-nagel.com>
2025-05-08 09:52:46 +0100

    added filter

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -8,6 +8,7 @@ import { IProduct } from './product.model';
 })
 export class CatalogComponent {
   products: IProduct[] = [];
+  filter: string = '';
 
   constructor() {
     this.products = [
@@ -189,5 +190,11 @@ export class CatalogComponent {
 
   getImageUrl(product: IProduct): string {
     return `/assets/images/robot-parts/${product.imageName}`;
+  }
+
+  getFilteredProducts(): IProduct[] {
+    return this.filter === ''
+      ? this.products
+      : this.products.filter((product) => product.category === this.filter);
   }
 }
